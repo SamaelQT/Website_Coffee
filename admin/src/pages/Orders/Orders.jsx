a6761af5d387508c33176967c1cf56571a4de871
SamaelQT <128588284+SamaelQT@users.noreply.github.com>
2025-05-02 11:13:37 +0700

    Update Orders.jsx
    đảo ngược, để order mới nhất trên cùng

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -12,7 +12,7 @@ const Orders = ({ url }) => {
   const fetchAllOrders = async () => {
     const response = await axios.get(url + "/api/order/list");
     if (response.data.success) {
-      setOrders(response.data.data);
+      setOrders(response.data.data.reverse());
       console.log(response.data.data);
     } else {
       toast.error("Error")
